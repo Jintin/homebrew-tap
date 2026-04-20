cb0e6afb4c8b296a5234980750aa4e89e47dd52b
Jintin <jintin.lin1018@gmail.com>
2026-04-20 23:28:08 +0800

    feat: add aliasme formula

--- Formula/aliasme.rb
+++ Formula/aliasme.rb
@@ -1,9 +1,9 @@
-class AliasmeLocal < Formula
-  desc "Organize your alias in command line (Local Test)"
+class Aliasme < Formula
+  desc "Organize your alias in command line"
   homepage "https://github.com/Jintin/aliasme"
-  url "file:///Users/ting/developer/aliasme.tar.gz"
+  url "https://github.com/Jintin/aliasme/archive/refs/tags/v3.1.0.tar.gz"
   sha256 "734c88140eb4f556d788568715815ee12a1a62cb7dc8ca751a0b45a4f3f5ee93"
-  version "3.0.0-local"
+  license "MIT"
 
   def install
     libexec.install "aliasme.sh"
@@ -14,5 +14,9 @@ class AliasmeLocal < Formula
       To finish the installation, add the following line to your shell profile (~/.bash_profile or ~/.zshrc):
         source #{opt_libexec}/aliasme.sh
     EOS
+  end
+
+  test do
+    assert_match "aliasme", shell_output("cat #{opt_libexec}/aliasme.sh")
   end
 end
